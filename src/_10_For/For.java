1bd73c543b315609b6ed06356c0074bd2f68fa4b
dongyoon7212 <dongyoon7212@naver.com>
2025-10-17 10:38:35 +0900

    Feat: Switch Case

---
+++ src/_10_For/For.java
@@ -0,0 +1,12 @@
+package _10_For;
+
+public class For {
+    public static void main(String[] args) {
+        //반복문 - for
+        /*
+        * for (선언; 조건; 증감) {
+        *
+        * }
+        * */
+    }
+}
