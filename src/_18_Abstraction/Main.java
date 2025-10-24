f5595b82a78d6e7ad8c778124e54bae537325d6e
dongyoon7212 <dongyoon7212@naver.com>
2025-10-24 16:51:43 +0900

    Feat: abstract

---
+++ src/_18_Abstraction/Main.java
@@ -0,0 +1,36 @@
+package _18_Abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        PhoneFactory phoneFactory1 = new PhoneFactory();
+        phoneFactory1.setFactoryName("애플 공장");
+        phoneFactory1.produce("iPhone17");
+        phoneFactory1.manage();
+        phoneFactory1.displayInfo();
+
+        Factory factory = new Factory() {
+            @Override
+            public void produce(String model) {
+                System.out.println(this.getFactoryName() + "에서 [ " + model + " ]을 생산합니다.");
+            }
+
+            @Override
+            public void manage() {
+                System.out.println(super.getFactoryName() + "을 관리합니다.");
+            }
+        };
+        //위와 같이 추상클래스의 개념을 벗어나는 객체 생성은 익명 클래스라는 개념 덕분이다.
+        //즉 재사용하지 않고 한 번만 사용하고 버릴거라면(즉 Main클래스에서만 사용하고 다른 곳에서는
+        //사용하지 않을거라면) 익명 클래스가 유용하다.
+        /*
+        * 그렇다고 해서 추상클래스 Factory의 객체가 생성된 것이 아닌 임시(즉석)로 만들어진 이름없는
+        * 자식클래스이다.
+        * 이름없는 자식클래스 == 익명 클래스
+        * 그래서 추상클래스의 객체가 만들어진 것이 아니지만 추상클래스의 개념은 지키고 있다.
+        * */
+
+        PhoneFactory phoneFactory2 = new PhoneFactory();
+        phoneFactory2.setFactoryName("삼성 공장");
+        phoneFactory2.produce("갤럭시S 25");
+    }
+}
---
+++ src/_18_Abstraction/TabletFactory.java
@@ -0,0 +1,18 @@
+package _18_Abstraction;
+
+/*
+* 문제
+* Factory 추상 클래스 상속받을것
+* 고유 메소드로 upgrade(String model)을 정의
+* 예) 객체명.upgrade("아이패드 프로 13인치 8세대");로 호출하면
+* "아이패드 프로 13인치 8세대로 업그레이드 합니다"출력
+* produce()/manage()메소드를 태블릿 공장에 맞게 적절히 수정할 것
+* (PhoneFactory 참고)
+* 메인 실행부에서 tabletFactory1 객체 생성하고 애플 태블릿 공장으로 이름짓기
+* produce("아이패드 미니 6세대")를 호출
+* manage()호출
+* upgrade("아이패드 미니 7세대")호출
+* */
+public class TabletFactory {
+}
+//상속 - 다형성(캐스팅) - 추상클래스 - 인터페이스 - 내부클래스 - 익명
