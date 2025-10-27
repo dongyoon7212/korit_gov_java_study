d247e9ff228f8faec8d072e6d438b04836b2f4db
dongyoon7212 <dongyoon7212@naver.com>
2025-10-27 16:29:38 +0900

    Feat: casting

--- src/_20_Casting/Shape/Circle.java
+++ src/_20_Casting/Shape/Circle.java
@@ -2,7 +2,7 @@ package _20_Casting.Shape;
 /*
 * 속성으로 double r (반지름)
 * allArgs
-* area() => 원의 넓이 구하는 공식
+* area() => 원의 넓이 구하는 공식 (원주율 3.14)
 * drawInternal() => 출력 (원 그리기 r = **)
 * */
 public class Circle {
