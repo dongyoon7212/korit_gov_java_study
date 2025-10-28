f61ce2b2f54d5069d7ceba82142261838177bc66
dongyoon7212 <dongyoon7212@naver.com>
2025-10-28 14:13:29 +0900

    Feat: pom.xml

---
+++ src/_22_Bean/BoardEntity.java
@@ -0,0 +1,69 @@
+package _22_Bean;
+
+import java.time.LocalDateTime;
+
+public class BoardEntity {
+    private int boardId;
+    private String boardTitle;
+    private String boardContent;
+    private int userId;
+    private String username;
+    private LocalDateTime createDt;
+    private LocalDateTime updateDt;
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(int boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getBoardTitle() {
+        return boardTitle;
+    }
+
+    public void setBoardTitle(String boardTitle) {
+        this.boardTitle = boardTitle;
+    }
+
+    public String getBoardContent() {
+        return boardContent;
+    }
+
+    public void setBoardContent(String boardContent) {
+        this.boardContent = boardContent;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDateTime getCreateDt() {
+        return createDt;
+    }
+
+    public void setCreateDt(LocalDateTime createDt) {
+        this.createDt = createDt;
+    }
+
+    public LocalDateTime getUpdateDt() {
+        return updateDt;
+    }
+
+    public void setUpdateDt(LocalDateTime updateDt) {
+        this.updateDt = updateDt;
+    }
+}
---
+++ src/_22_Bean/Main.java
@@ -0,0 +1,23 @@
+package _22_Bean;
+
+public class Main {
+    public static void main(String[] args) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(1);
+        userEntity.setUsername("이동윤");
+        userEntity.setAge(27);
+        userEntity.setEmail("dongyoon7212@naver.com");
+        System.out.println(userEntity);
+
+        /*
+        * 게시물 엔티티
+        * 게시물 아이디
+        * 게시물 제목
+        * 게시물 내용
+        * 작성자 아이디
+        * 작성자 닉네임
+        * 생성시각
+        * 수정시각
+        * */
+    }
+}
