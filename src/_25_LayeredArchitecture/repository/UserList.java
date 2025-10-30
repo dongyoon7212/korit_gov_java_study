7a7c1a9254a30dced777a2d38ea8096aaf52976d
dongyoon7212 <dongyoon7212@naver.com>
2025-10-30 15:26:44 +0900

    Feat: layered 로그인 회원가입

--- src/_25_LayeredArchitecture/repository/UserList.java
+++ src/_25_LayeredArchitecture/repository/UserList.java
@@ -9,6 +9,10 @@ public class UserList {
         this.users = new User[0];
     }
 
+    public User[] getAllUser() {
+        return users;
+    }
+
     public User findByUsername(String username) {
         for (User user : users) {
             if (user.getUsername().equals(username)) {
@@ -16,5 +20,25 @@ public class UserList {
             }
         }
         return null;
+    }
+
+    public int generateUserId() {
+        //만약 users 배열에 아무것도 없으면 1반환
+        //있으면 마지막 user객체의 userId에 +1 한 결과값을 반환
+        //삼항연산자
+        return users.length == 0 ? 1 : users[users.length - 1].getUserId() + 1;
+    }
+
+    public void add(User user) {
+        //새로 넣을 배열 => 길이? => 원래 있던 배열의 길이에서 + 1
+        User[] temp = new User[users.length + 1];
+        //새로 넣을 배열에 원래 있던 요소들을 먼저 추가
+        for (int i = 0; i < users.length; i++) {
+            temp[i] = users[i];
+        }
+        //새로 추가할 user를 넣어야함
+        temp[temp.length - 1] = user;
+        //users에 있던 배열에서 새로운 배열로 바꾸기
+        users = temp;
     }
 }
--- src/_25_LayeredArchitecture/service/UserService.java
+++ src/_25_LayeredArchitecture/service/UserService.java
@@ -1,5 +1,6 @@
 package _25_LayeredArchitecture.service;
 
+import _25_LayeredArchitecture.dto.SigninReqDto;
 import _25_LayeredArchitecture.dto.SignupReqDto;
 import _25_LayeredArchitecture.entity.User;
 import _25_LayeredArchitecture.repository.UserList;
@@ -9,6 +10,19 @@ public class UserService {
 
     public UserService(UserList userList) {
         this.userList = userList;
+    }
+
+    public User signin(SigninReqDto signinReqDto) {
+        User foundUser = userList.findByUsername(signinReqDto.getUsername());
+        if (foundUser == null) {
+            //username을 가진 회원 없음
+            return null;
+        }
+        if (!foundUser.getPassword().equals(signinReqDto.getPassword())) {
+            //비밀번호가 일치하지 않음
+            return null;
+        }
+        return foundUser;
     }
 
     //username 중복확인
@@ -21,6 +35,16 @@ public class UserService {
         //1. signupReqDto -> user (entity)
         User user = signupReqDto.toEntity();
         //2. userId 생성
+        user.setUserId(userList.generateUserId());
         //3. user 배열에 추가
+        userList.add(user);
+    }
+
+    //회원정보 전체 출력
+    public void printAllUserList() {
+        System.out.println("[[ 회원정보 전체 조회 ]]");
+        for (User user : userList.getAllUser()) {
+            System.out.println(user);
+        }
     }
 }
