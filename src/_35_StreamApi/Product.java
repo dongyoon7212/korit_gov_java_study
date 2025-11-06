0eb3add8b0b6d58f76d87b40d8d73c2db9c6f1e7
dongyoon7212 <dongyoon7212@naver.com>
2025-11-06 15:37:56 +0900

    END

--- src/_35_StreamApi/Product.java
+++ src/_35_StreamApi/Product.java
@@ -1,4 +1,11 @@
 package _35_StreamApi;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Product {
+    private String name;
+    private Integer price;
 }
--- src/_35_StreamApi/ProductMain.java
+++ src/_35_StreamApi/ProductMain.java
@@ -2,6 +2,7 @@ package _35_StreamApi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /*
 * product => name, price
@@ -11,6 +12,7 @@ import java.util.List;
 * */
 public class ProductMain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         List<Product> products = Arrays.asList(
                 new Product("삼성 갤럭시북3 프로", 1850000),
                 new Product("삼성 갤럭시북 이온", 1350000),
@@ -23,5 +25,23 @@ public class ProductMain {
                 new Product("LG 그램 17", 2190000),
                 new Product("LG 울트라PC", 1140000)
         );
+
+        System.out.print("브랜드 명을 입력하세요: ");
+        String brandName = scanner.nextLine();
+
+        System.out.println("최대 가격을 입력하세요: ");
+        int maxPrice = scanner.nextInt();
+
+        System.out.println("[ 필터링 결과 ]");
+        List<Product> filtered = products.stream()
+                .filter(product -> product.getName().contains(brandName))
+                .filter(product -> product.getPrice() <= maxPrice)
+                .toList();
+
+        if (filtered.isEmpty()) {
+            System.out.println("해당 조건에 맞는 제품이 없습니다.");
+        } else {
+            filtered.forEach(System.out::println);
+        }
     }
 }
