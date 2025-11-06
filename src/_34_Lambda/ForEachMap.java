c07f114df48ab24d85e27e79c8104d97d0c3d8bc
dongyoon7212 <dongyoon7212@naver.com>
2025-11-06 11:05:14 +0900

    Feat: lambda

---
+++ src/_34_Lambda/ForEachMap.java
@@ -0,0 +1,9 @@
+package _34_Lambda;
+
+//1. 스캐너 객체선언과 입력받아 저장할 Map선언(String, Integer)
+//2. 상품을 입력할 갯수를 count로 입력받기(nextInt로)
+//3. count만큼 반복하며 상품명(키)과 가격(값)으로 각각 입력받고 Map변수에 추가
+//4. [상품 목록] 출력 후 람다식을 이용해서 forEach로 [상품명]: ***, [가격]: ***원 이런식으로 출력
+
+public class ForEachMap {
+}
--- src/_34_Lambda/stringProcessor/StringProcessor.java
+++ src/_34_Lambda/stringProcessor/StringProcessor.java
@@ -1,4 +1,6 @@
 package _34_Lambda.stringProcessor;
 
+@FunctionalInterface
 public interface StringProcessor {
+    String process(String input);
 }
